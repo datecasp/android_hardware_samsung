d5c2f1803cab20c27f457e100e88cb4249d931e4
datecasp <54754720+datecasp@users.noreply.github.com>
2021-06-14 11:10:15 +0200

    Update BootCompletedReceiver.java

--- AdvancedDisplay/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ AdvancedDisplay/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -15,7 +15,8 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.device;
+//package org.lineageos.settings.device;
+package org.du.settings.device;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
