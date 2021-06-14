f680b11f6b4c3751eca06424efd2eefcd91cbe3a
datecasp <54754720+datecasp@users.noreply.github.com>
2021-06-14 11:07:03 +0200

    Update AdvancedDisplayActivity.java

--- AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayActivity.java
+++ AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayActivity.java
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.device;
+//package org.lineageos.settings.device;
+package org.du.settings.device;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
